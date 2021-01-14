aeb1eeefdc6ebfb365c83f9e8cc615a12532a060
Imani R Dillahunt <imanirdillahunt@Imanis-MacBook-Air.local>
2021-01-14 09:07:14 -0500

    color brackets

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 $(document).ready(function (){
 
-/// Global variable declarations
+//Open array for user input 
 var cityList = [];
+
+
 var cityname;
 
-// local storage functions
+// functions to display in local storage
 lsCityList();
 lsWeather();
 
 
-// This function displays the city entered by the user into the DOM
+//What city the user enters will show up in the DOM
+
 function renderCities(){
     $("#cityList").empty();
     $("#cityInput").val("");
+
+
     
     for (i=0; i<cityList.length; i++){
         var a = $("<a>");
@@ -88,16 +93,15 @@ $("#cityInput").keypress(function(o){
 })
 
 // This function runs the Open Weather API AJAX call and displays the current city, weather, and 5 day forecast to the DOM
-async function displayWeather() {
+ function displayWeather() {
 
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&units=imperial&appid=03a3736e09588483eda83a7891f2b76e";
 
-    var response = await $.ajax({
+     $.ajax({
         url: queryURL,
         method: "GET"
-      })
+      }).then(function(response){
         console.log(response);
-
         var WeatherDiv = $("<div class='card-body' id='currentWeather'>");
         var getCity = response.name;
         var date = new Date();
@@ -118,72 +122,82 @@ async function displayWeather() {
         WeatherDiv.append(windSpeed);
         var getLong = response.coord.lon;
         var getLat = response.coord.lat;
-        
+
+
+
         var locationURL = "https://api.openweathermap.org/data/2.5/uvi?appid=03a3736e09588483eda83a7891f2b76e&lat="+getLat+"&lon="+getLong;
-        var uvResponse = await $.ajax({
+         $.ajax({
             url: locationURL,
             method: "GET"
+        }).then(function(uvResponse){
+            var getlocIndex = uvResponse.value;
+            var locNumber = $("<span>");
+            if (getlocIndex > 0 && getlocIndex <= 2.99){
+                locNumber.addClass("low");
+            }else if(getlocIndex >= 3 && getlocIndex <= 5.99){
+                locNumber.addClass("moderate");
+            }else if(getlocIndex >= 6 && getlocIndex <= 7.99){
+                locNumber.addClass("high");
+            }else if(getlocIndex >= 8 && getlocIndex <= 10.99){
+                locNumber.addClass("vhigh");
+            }else{
+                locNumber.addClass("extreme");
+            } 
+            locNumber.text(getlocIndex);
+            var locIndex = $("<p class='card-text'>").text("UV Index: ");
+            locNumber.appendTo(locIndex);
+            WeatherDiv.append(locIndex);
+            $("#weatherContainer").html(WeatherDiv);
         })
 
+      })
+        
+
+        
+        
         // getting UV Index info and setting color class according to value
-        var getlocIndex = uvResponse.value;
-        var locNumber = $("<span>");
-        if (getlocIndex > 0 && getlocIndex <= 2.99){
-            locNumber.addClass("low");
-        }else if(getlocIndex >= 3 && getlocIndex <= 5.99){
-            locNumber.addClass("moderate");
-        }else if(getlocIndex >= 6 && getlocIndex <= 7.99){
-            locNumber.addClass("high");
-        }else if(getlocIndex >= 8 && getlocIndex <= 10.99){
-            locNumber.addClass("vhigh");
-        }else{
-            locNumber.addClass("extreme");
-        } 
-        locNumber.text(getlocIndex);
-        var locIndex = $("<p class='card-text'>").text("UV Index: ");
-        locNumber.appendTo(locIndex);
-        WeatherDiv.append(locIndex);
-        $("#weatherContainer").html(WeatherDiv);
+        
 }
 
 // This function runs the AJAX call for the 5 day forecast and displays them to the DOM
-async function displayFiveDayForecast() {
+ function displayFiveDayForecast() {
 
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q="+cityname+"&units=imperial&appid=03a3736e09588483eda83a7891f2b76e";
 
-    var response = await $.ajax({
+    $.ajax({
         url: queryURL,
         method: "GET"
+      }).then(function(response){
+        var forecast = $("<div  id='fiveDayForecast'>");
+        var fcHeader = $("<h5 class='card-header border-secondary'>").text("5 Day Forecast");
+        forecast.append(fcHeader);
+        var fiveDaySection = $("<div  class='card-deck'>");
+        forecast.append(fiveDaySection);
+        
+        console.log(response);
+        for (i=0; i<5;i++){
+            var forecastSection = $("<div class='card mb-3 mt-3'>");
+            var fcBody = $("<div class='card-body'>");
+            var date = new Date();
+            var calendar=(date.getMonth()+1)+"/"+(date.getDate()+i+1)+"/"+date.getFullYear();
+            var forecastDate = $("<h5 class='card-title'>").text(calendar);
+            
+          fcBody.append(forecastDate);
+          var getWeatherIcon = response.list[i].weather[0].icon;
+          console.log(getWeatherIcon);
+          var displayWeatherIcon = $("<img src = http://openweathermap.org/img/wn/" + getWeatherIcon + ".png />");
+          fcBody.append(displayWeatherIcon);
+          var getTemp = response.list[i].main.temp;
+          var temp = $("<p class='card-text'>").text("Temp: "+getTemp+"° F");
+          fcBody.append(temp);
+          var getHumidity = response.list[i].main.humidity;
+          var humidityp = $("<p class='card-text'>").text("Humidity: "+getHumidity+"%");
+          fcBody.append(humidityp);
+          forecastSection.append(fcBody);
+          fiveDaySection.append(forecastSection);
+        }
+        $("#forecastContainer").html(forecast);
       })
-      var forecast = $("<div  id='fiveDayForecast'>");
-      var fcHeader = $("<h5 class='card-header border-secondary'>").text("5 Day Forecast");
-      forecast.append(fcHeader);
-      var fiveDaySection = $("<div  class='card-deck'>");
-      forecast.append(fiveDaySection);
-      
-      console.log(response);
-      for (i=0; i<5;i++){
-          var forecastSection = $("<div class='card mb-3 mt-3'>");
-          var fcBody = $("<div class='card-body'>");
-          var date = new Date();
-          var calendar=(date.getMonth()+1)+"/"+(date.getDate()+i+1)+"/"+date.getFullYear();
-          var forecastDate = $("<h5 class='card-title'>").text(calendar);
-          
-        fcBody.append(forecastDate);
-        var getWeatherIcon = response.list[i].weather[0].icon;
-        console.log(getWeatherIcon);
-        var displayWeatherIcon = $("<img src = http://openweathermap.org/img/wn/" + getWeatherIcon + ".png />");
-        fcBody.append(displayWeatherIcon);
-        var getTemp = response.list[i].main.temp;
-        var temp = $("<p class='card-text'>").text("Temp: "+getTemp+"° F");
-        fcBody.append(temp);
-        var getHumidity = response.list[i].main.humidity;
-        var humidityp = $("<p class='card-text'>").text("Humidity: "+getHumidity+"%");
-        fcBody.append(humidityp);
-        forecastSection.append(fcBody);
-        fiveDaySection.append(forecastSection);
-      }
-      $("#forecastContainer").html(forecast);
     }
 
 // This function is used to pass the city in the history list to the displayWeather function
